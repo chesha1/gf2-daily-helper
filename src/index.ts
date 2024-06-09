decf32ef9985a7141aef44eebac9126aa4dbda75
chesha1 <chesha1@163.com>
2024-06-09 22:11:01 +0800

    feat: 登录和兑换物品的功能

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,67 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import axios, { AxiosResponse, AxiosRequestHeaders } from 'axios';
+import MD5 from 'crypto-js/md5';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	PASSWORD: string;
+	ACCOUNT_NAME: string;
+}
+
+interface loginPayload {
+	account_name: string;
+	passwd: string;
+	source: string;
+}
+
+interface exchangeRequestBody {
+	exchange_id: number;
+}
+
+// 登录，然后提取出 jwt
+async function Login(payload: loginPayload): Promise<string> {
+	try {
+		const response: AxiosResponse = await axios.post('https://gf2-bbs-api.sunborngame.com/login/account', payload);
+		return response.data.data.account.token as string;
+	}
+	catch (error) {
+		if (axios.isAxiosError(error)) {
+			return error.response?.data;
+		}
+		else {
+			return 'An unknown error occurred';
+		}
+	}
+}
+
+// 兑换物品
+async function ExchangeItem(exchange_id: number, token: string) {
+	const requestBody: exchangeRequestBody = {
+		exchange_id: exchange_id,
+	};
+	const requestHeader = {
+		Authorization: token,
+	} as AxiosRequestHeaders;
+	try {
+		const response: AxiosResponse = await axios.post('https://gf2-bbs-api.sunborngame.com/community/item/exchange', requestBody, {
+			headers: requestHeader
+		});
+		return response.data;
+	}
+	catch (error) {
+		return error;
+	}
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const userPayload: loginPayload = {
+			account_name: env.ACCOUNT_NAME,
+			passwd: MD5(env.PASSWORD).toString(),
+			source: 'phone',
+		};
+		const jwtToken: string = await Login(userPayload);
+		const data = await ExchangeItem(5, jwtToken);
+		const response = JSON.stringify(data);
+
+		return new Response(response);
 	},
 };
