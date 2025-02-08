a08e60587fafbe100e9325c8adad506b1e5ed6ef
chesha1 <chesha1@163.com>
2025-02-08 14:33:22 +0800

    chore: eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,21 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {languageOptions: { globals: globals.browser },
-   rules:{
-  'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
-  'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1, 'maxBOF': 0 }],
-  'prefer-const': ['off'],
-  '@typescript-eslint/no-unused-vars': ['off']
-  
-}}
+  ...tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+  ),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+      'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1, 'maxBOF': 0 }],
+      'prefer-const': ['off'],
+    },
+  },
+  // do not use Stylistic, as it conflicts with typescript-eslint
+  // especially with the indent rules
+  // see also: https://github.com/typescript-eslint/typescript-eslint/issues/1824
 ];
